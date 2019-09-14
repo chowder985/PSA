a2a9b0ca5fbc3a9b1bd4a2f93f4f84f21a1d9c1f
AlexMorrisJPurdue2023 <54811902+AlexMorrisJPurdue2023@users.noreply.github.com>
2019-09-14 16:01:25 -0400

    Update DemoApplication.java

--- DemoApplication.java
+++ DemoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class User {
 //Course is an object
 	 private String userName;
 	 private String userMajor;
