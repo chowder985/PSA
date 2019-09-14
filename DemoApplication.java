125464db68873990a93719b29e2d338a8bb76020
AlexMorrisJPurdue2023 <54811902+AlexMorrisJPurdue2023@users.noreply.github.com>
2019-09-14 15:26:23 -0400

    Update DemoApplication.java

--- DemoApplication.java
+++ DemoApplication.java
@@ -5,6 +5,40 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+private String userName;
+	 private String userMajor;
+	 private String userYear;
+
+
+     public User(String name, String Major, String year)  {
+		userName = name;
+		userMajor = major;
+		userYear = year;
+	 }
+
+    public string getName () {
+		return userName;
+	}
+ 
+	public string getMajor() {
+		return getMajor;
+	}
+	public string getYear() {
+		return userYear;
+	}
+
+	public void changeName (String x) {
+		userName = x;
+	}
+ 
+	public void changeMajor(String y) {
+		userMajor = y;
+	}
+	public void changeYear(String z) {
+		userYear = z;
+	}
+
+
 
 	public static void main(String[] args) {
 
