fcf187058ec8871755ea6216128180b161eb6a3d
AlexMorrisJPurdue2023 <54811902+AlexMorrisJPurdue2023@users.noreply.github.com>
2019-09-14 15:25:48 -0400

    Add files via upload

---
+++ DemoApplication.java
@@ -0,0 +1,25 @@
+package com.purduestudyapp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+
+     
+
+
+
+
+
+
+
+
+
+
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
