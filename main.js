2763c18415fb0fae771a1c42eff6fa2aae6e16b6
ilbit01 <lee3252@purdue.edu>
2019-09-15 01:29:16 -0400

    Course update

--- main.js
+++ main.js
@@ -1,11 +1,73 @@
 var full_name;
+var courseArray = ["CS 180", "MA 161", "SCLA 101", "ENGL 106"];
 // Firebase App (the core Firebase SDK) is always required and
 // must be listed before other Firebase SDKs
-var firebase = require("firebase/app");
+//var firebase = require("firebase/app");
 
 // Add the Firebase products that you want to use
-require("firebase/auth");
-require("firebase/firestore");
+// require("firebase/auth");
+// require("firebase/firestore");
+
+$(document).ready((event) => {
+    if (localStorage.getItem("last")) {
+        document.getElementById(localStorage.getItem("last")).style.display = 'block';
+    } else {
+        document.getElementById('Profile').style.display = 'block';
+        localStorage.setItem("last", "Profile");
+    }
+
+    full_name = document.getElementById("full_name");
+    if (full_name) {
+        $.get("https://jsonplaceholder.typicode.com/todos/1", function (data, status) {
+            full_name.innerHTML = "" + data.title
+        });
+    }
+
+    for (i = 0; i < courseArray.length; i++) {
+        $("ul").append("<li><span class='trash'><i class='fa fa-trash'></i></span>" + courseArray[i] + "</li>");
+        console.log(courseArray);
+    }
+
+    // $(".course-list").click(function (e) {
+    //     console.log(e.target.text);
+    // });
+
+    // $(".delete-course").click((e) => {
+    //     console.log(courseArray[e.target.id]);
+    //     courseArray.splice(e.target.id, 1);
+    //     console.log(courseArray);
+    //     renderCourseList();
+    // });
+
+
+
+    $("ul").on("click", "li", function () {
+        $(this).toggleClass("completed");
+    });
+
+    $("ul").on("click", ".trash", function (event) {
+        $(this).parent().fadeOut(500, function () {
+            courseArray.splice(courseArray.indexOf($(this).text()), 1);
+            console.log($(this).text());
+            console.log(courseArray);
+            $(this).remove();
+        });
+        event.stopPropagation();
+    });
+
+    $("input[type='text']").keypress(function (event) {
+        if (event.which === 13) {
+            var todoText = $(this).val();
+            $(this).val("");
+            courseArray.push(todoText);
+            $("ul").append("<li><span class='trash'><i class='fa fa-trash'></i></span>" + todoText + "</li>");
+        }
+    });
+
+    $(".fa-plus").click(function () {
+        $("input[type='text']").fadeToggle();
+    });
+});
 
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
@@ -32,20 +94,9 @@ function showPage(shown) {
     localStorage.setItem("last", shown);
 }
 
-window.addEventListener('DOMContentLoaded', (event) => {
-    if (localStorage.getItem("last")) {
-        document.getElementById(localStorage.getItem("last")).style.display = 'block';
-    } else {
-        document.getElementById('Profile').style.display = 'block';
-        localStorage.setItem("last", "Profile");
-    }
-
-    full_name = document.getElementById("full_name");
-    if (full_name) {
-        $.get("https://jsonplaceholder.typicode.com/todos/1", function (data, status) {
-            full_name.innerHTML = "" + data.title
-        });
-    }
-
-
-});
+// function renderCourseList() {
+//     $('li').remove();
+//     for (i = 0; i < courseArray.length; i++) {
+//         $('ul').append("<li><a class='course-list' href='#'>" + courseArray[i] + "</a> - <button class='delete-course' id='" + i + "'>-</button></li>");
+//     }
+// }
